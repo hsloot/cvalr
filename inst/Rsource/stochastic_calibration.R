8c9cae2b8ea299186e5592f3615251029ae432a5
Henrik Sloot <henrik.sloot@gmail.com>
2021-02-08 10:28:47 +0100

    add stochastic calibration script

--- inst/Rsource/stochastic_calibration.R
+++ inst/Rsource/stochastic_calibration.R
@@ -1,35 +1,47 @@
-suniroot <- function(
-    f, interval, is_increasing, ...,
-    lower = min(interval), upper = max(interval),
-    tolerance = .Machine$double.eps^0.25,
-    max_iter = 1e3,
-    p = 0.6, grid_length = 1e2) {
-  interval <- c(lower, upper)
-  stopifnot(isTRUE(all(is.finite(interval))))
-  normalize <- function(x) { x / sum(x) }
-
-  atoms <- seq(lower, upper, length.out = grid_length)
-  weights <-  normalize(rep(1, grid_length))
-  qf <- q.r(DiscreteDistribution(supp = atoms, prob = weights))
-
-  iter <- 0
-  while (!isTRUE(abs(qf(1 - 0.05/2) - qf(0.05/2)) < tolerance) && iter < max_iter) {
-    x <- qf(0.5)
-    z <- f(x, ...)
-    y <- ((z < 0) * 2 - 1) * (is_increasing * 2 - 1)
-
-    i <- min(which(atoms >= x))
-    atoms <- c(atoms, x)
-    weights <- c(weights, mean(weights[c(i, i+1)]))
-    sort_ind <- order(atoms)
-    atoms <- atoms[sort_ind]
-    weights <- weights[sort_ind]
-    weights[y*(atoms  - x) > 0] <- weights[y*(atoms  - x) > 0] * p
-    weights[y*(atoms - x) <= 0] <- weights[y*(atoms - x) <= 0] * (1-p)
-    weights <- normalize(weights)
-    qf <- q.r(DiscreteDistribution(supp = atoms, prob = weights))
-    iter <- iter + 1
-  }
-
-  qf(0.5)
+#' @importFrom rmo rexmo_markovian ex_intensities_alpha_stable
+cdo_tranche_expected_loss_exmo_alpha_stable_mc <- function(
+    times, lower, upper, recovery_rate, lambda, nu, d = 75, n = 1e3) {
+  alpha <- log2(2 - nu)
+  tau <- rmo::rexmo_markovian(n, d, ex_intensities_alpha_stable(d, alpha)) / lambda
+  sapply(times, function(t) {
+    mean(pmin(
+      pmax(
+        (1 - recovery_rate) * apply(tau <= t, 1, mean) - lower,
+        0
+      ),
+      upper - lower
+    ))
+  })
 }
+
+cdo_fitness_mc <- function(
+    nu,
+    lambda,
+    times,
+    discount_factors,
+    lower, upper, recovery_rate,
+    coupon, upfront,
+    d = 75, n = 1e3,
+    expected_losses_fn = cdo_tranche_expected_loss_exmo_alpha_stable_mc,
+    norm_fn = function(x) { sqrt(sum(x^2)) }) {
+  cdo_equations <- Vectorize(
+    function(nu, lambda, lower, upper, recovery_rate, coupon, upfront) {
+      expected_losses <- expected_losses_fn(times, lower, upper, recovery_rate,
+        lamnda, nu, d, n)
+      cdo_equation(
+        expected_losses = expected_losses,
+        times = times,
+        discount_factors = discount_factors,
+        lower = lower,
+        upper = upper,
+        coupon = coupon,
+        upfront = upfront
+      )
+    },
+    vectorize.args = c("lower", "upper", "recovery_rate", "coupon", "upfront")
+  )
+
+  -norm_fn(cdo_equations(nu, lambda, upper, recovery_rate, coupon, upfront))
+}
+
+cdo_calibration
